90aa413882f87616f62dadb86b8b969eafd7ac63
eithompson <eric.ian.thompson@gmail.com>
2020-05-10 21:01:46 -0700

    working

--- barebones/sylloge.py
+++ barebones/sylloge.py
@@ -6,7 +6,7 @@ from datetime import datetime
 now = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 if len(argv) != 4: # argv[0] is filename
     raise Exception("Supply three arguments: search term and directory of corpus")
-search_term = argv[1].lower()
+search_term = argv[1]
 corpus = argv[2]
 corpus_dir = argv[3]
 
@@ -38,18 +38,21 @@ def single_source_selections(content, search_term):
     hits = [idx for idx, val in enumerate(content) if search_term.lower() in val.lower()]
     if hits == []:
         return None
-    context = [max(0, hit - 1) for hit in hits] + [min(len(hits), hit + 1) for hit in hits]  
+    print(hits)
+    context = [max(0, hit - 1) for hit in hits] + [min(len(content) - 1, hit + 1) for hit in hits]  
+    print(context)
     # combine, dedupe, order
     hits_with_context = sorted(list(set(hits + context)))
-
+    print(hits_with_context)
     # now, find our selections
     selections_list = []
     for idx in range(len(hits_with_context)):
-        paragraph = content[idx]
+        paragraph_i = hits_with_context[idx]
+        paragraph = content[paragraph_i]
         if selections_list == []:
             # starting
             selections_list.append([paragraph])
-        elif hits_with_context[idx - 1] == idx - 1:
+        elif hits_with_context[idx - 1] == paragraph_i - 1:
             # still in same selection as last time. add to it.
             selections_list[-1].append(paragraph)
         else:
@@ -59,16 +62,18 @@ def single_source_selections(content, search_term):
     return selections_list
 
 # build markdown.
-md = "# " + search_term + "\n"
+# i thought we'd already have newlines from readlines() in ssc_parse() but guess not;
+# adding a lot here
+md = "# " + search_term + "\n\n"
 for filename in listdir(corpus_dir):
     parsed = parse_file(corpus_dir + "/" + filename, corpus)
     selections = single_source_selections(parsed["body"], search_term) 
     if selections is not None:
         url = parsed["url"][0:-1] # don't want newline
-        md = md + "## " + parsed["title"] + "[" + url + "]\n"
+        md = md + "## " + parsed["title"] + "[" + url + "]\n\n"
 
         for idx in range(len(selections)):
-            md = md + "### Selection " + str(idx + 1) + "\n" + "".join(selections[idx])  
+            md = md + "### Selection " + str(idx + 1) + "\n\n" + "\n".join(selections[idx]) + "\n"
 
 with open(output_filepath, "w") as f:
    f.write(md) 
