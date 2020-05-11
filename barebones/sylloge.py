01fec74bbb5841a5512de1ae2326dc2ba9dc5e44
eithompson <eric.ian.thompson@gmail.com>
2020-05-10 19:28:25 -0700

    add selection search logic

--- barebones/sylloge.py
+++ barebones/sylloge.py
@@ -22,6 +22,37 @@ def parse_file(filepath, corpus):
        parsed_dict = parse_ssc(filepath) 
    else:
        raise ValueError("Please supply valid corpus")
+    return parsed_dict
+
+def find_selections(content, search_term):
+    '''
+    Takes a list of one source's paragraphs.
+    Outputs a list of lists of the selections that match the search term, context included.
+    Adjacent hit-paragraphs are concatenated into one selection.
+    '''
+    hits = [idx for idx, val in enumerate(content) if search_term.lower() in val.lower()]
+    if hits == []:
+        return None
+    context = [max(0, hit - 1) for hit in hits] + [min(len(hits), hit + 1) for hit in hits]  
+    # combine, dedupe, order
+    hits_with_context = sorted(list(set(hits + context)))
+
+    # now, find our selections
+    selections_list = []
+    for idx in range(len(hits_with_context)):
+        paragraph = content[idx]
+        if selections_list == []:
+            # starting
+            selections_list.append([paragraph])
+        elif hits_with_context[idx - 1] == idx - 1:
+            # still in same selection as last time. add to it.
+            selections_list[-1].append(paragraph)
+        else:
+            # new selection
+            selections_list.append([paragraph])
+
+    return selections_list
+
 
 
 
